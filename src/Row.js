d4eea4e51c237062ef5bdd15963d90814536c6fa
Kajal Soni <kaajalsoni121@gmail.com>
2020-07-29 02:46:44 +0530

    second commit

---
+++ src/Row.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Row({title}){ //passing the props
+    return ( <div>
+    <h2>{title}</h2>
+    </div>)
+}
+
+export default Row
---
+++ src/requests.js
@@ -0,0 +1,16 @@
+const API_KEY ="19f84e11932abbc79e6d83f82d6d1045"
+
+
+
+const requests = {
+    fetchTrending: `/trending/all/day?api_key=${API_KEY}&language=en-US`, //end-points
+    fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
+    fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
+    fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_networks=28`,
+    fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_networks=35`,
+    fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_networks=27`,
+    fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_networks=10749`,
+    fetchDocumentaries: `/discover/movie?api_key=${API_KEY}&with_networks=99`,
+}
+
+export default requests
