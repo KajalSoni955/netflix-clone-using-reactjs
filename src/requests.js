c639fdc1946f2ef446278bae7801fe7ca970b4b2
Kajal Soni <kaajalsoni121@gmail.com>
2020-07-29 17:12:51 +0530

    4

--- src/requests.js
+++ src/requests.js
@@ -1,16 +1,14 @@
-const API_KEY ="19f84e11932abbc79e6d83f82d6d1045"
-
-
+const API_KEY = "19f84e11932abbc79e6d83f82d6d1045";
 
 const requests = {
-    fetchTrending: `/trending/all/day?api_key=${API_KEY}&language=en-US`, //end-points
-    fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
-    fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
-    fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_networks=28`,
-    fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_networks=35`,
-    fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_networks=27`,
-    fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_networks=10749`,
-    fetchDocumentaries: `/discover/movie?api_key=${API_KEY}&with_networks=99`,
-}
+  fetchTrending: `/trending/all/day?api_key=${API_KEY}&language=en-US`, //end-points
+  fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
+  fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
+  fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
+  fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
+  fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
+  fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
+  fetchDocumentaries: `/discover/movie?api_key=${API_KEY}&with_genres=99`,
+};
 
-export default requests
+export default requests;
