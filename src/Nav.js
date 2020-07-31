0b42376a863883817e7bd90d922b9834ef93c275
Kajal Soni <kaajalsoni121@gmail.com>
2020-08-01 02:47:56 +0530

    nav

---
+++ src/Nav.js
@@ -0,0 +1,21 @@
+import React from "react";
+import "./Nav.css";
+
+function Nav() {
+  return (
+    <div className="nav">
+      <img
+        className="nav__logo"
+        src="580b57fcd9996e24bc43c529.png"
+        alt="Netflix Logo"
+      ></img>
+      <img
+        className="nav__avatar"
+        src="0ddccae723d85a703b798a5e682c23c1.png"
+        alt="Netflix Logo"
+      ></img>
+    </div>
+  );
+}
+
+export default Nav;
